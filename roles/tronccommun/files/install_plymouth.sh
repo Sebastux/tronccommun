0bdb3f2fc774e2db6e5a6247707a875f15bd0500
Séba <sebmeloni59@gmail.com>
2023-01-08 12:37:05 +0100

    Création du script d'activation de plymouth

---
+++ roles/tronccommun/files/install_plymouth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+#==============================================================================
+# Titre            : install_plymouth.sh
+# Description      : Script de reconstruction du grub.
+# Auteur           : Sebastux
+# Date             : 08/01/2023
+# Modification     : 08/01/2023
+# Version          : 1.00
+# Utilisation      : ./install_plymouth.sh
+# Notes            : Ce script est destiné à être lancé par ansible pour
+#                    installer un théme Plymouth et reconstruire l'initrd
+#                    après installation.
+# Version de bash  : 5.2.15(1)-release
+#==============================================================================
+
+# Installation et activation du theme plymouth
+plymouth-set-default-theme solar --rebuild-initrd
